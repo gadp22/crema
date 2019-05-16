4fcd5ee63620a1ec1a5c1801be1bd37ebb4239cf
gadp22 <glorygadp@gmail.com>
2019-05-16 11:14:58 +0700

    [add] added capability to retrieve request parameters via request body (raw)

--- database.go
+++ database.go
@@ -7,10 +7,8 @@ package crema
 import (
 	"database/sql"
 	"fmt"
-	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
@@ -36,13 +34,13 @@ func InitDB() *sql.DB {
 
 	if driver == "psql" {
 
-		Printf("[DB_HELPER] Connecting to PostgreSQL DB ...")
+		Printf("[DATABASE] Connecting to PostgreSQL DB ...")
 
 		psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 			"password=%s dbname=%s sslmode=disable",
 			host, port, user, pass, dbname)
 
-		Printf("[DB_HELPER] " + psqlInfo)
+		Printf("[DATABASE] " + psqlInfo)
 
 		db, err := sql.Open("postgres", psqlInfo)
 
@@ -52,7 +50,7 @@ func InitDB() *sql.DB {
 
 		HandleError(err)
 
-		Printf("[DB_HELPER] Successfully connected!")
+		Printf("[DATABASE] Successfully connected!")
 
 		return db
 
@@ -102,149 +100,4 @@ func Scan(rows *sql.Rows) (interface{}, error) {
 	}
 
 	return allMaps, err
-}
-
-// DeleteData performs the standard delete mechanism
-// TO DO : documentaion will be updated soon
-func DeleteData(fn func(*sql.Tx, map[string]string) (sql.Result, error), w http.ResponseWriter, r *http.Request) (interface{}, int) {
-	Printf("[DATABASE_HELPER] started deleting data ...")
-	defer Printf("[DATABASE_HELPER] finished deleting data ...")
-
-	tx, err := BeginTransaction()
-
-	if err != nil {
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	var conditions map[string]string
-	conditions = make(map[string]string)
-
-	params := mux.Vars(r)
-
-	conditions = PopulateSingleValueQueries(r, conditions)
-	conditions = PopulateParams(params, conditions)
-
-	res, err := fn(tx, conditions)
-
-	if err != nil {
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	err = tx.Commit()
-
-	if err != nil {
-		tx.Rollback()
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	Printf(res)
-	return fmt.Sprintf("Data has been successfully deleted."), http.StatusOK
-}
-
-// PutData performs the standard put mechanism
-// TO DO : documentaion will be updated soon
-func PutData(fn func(*sql.Tx, map[string]string) (sql.Result, error), w http.ResponseWriter, r *http.Request) (interface{}, int) {
-	Printf("[DATABASE_HELPER] started updating data ...")
-	defer Printf("[DATABASE_HELPER] finished updating data ...")
-
-	tx, err := BeginTransaction()
-
-	if err != nil {
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	var values map[string]string
-	values = make(map[string]string)
-
-	params := mux.Vars(r)
-
-	values = PopulateSingleValueQueries(r, values)
-	values = PopulateParams(params, values)
-
-	res, err := fn(tx, values)
-
-	if err != nil {
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	err = tx.Commit()
-
-	if err != nil {
-		tx.Rollback()
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	Printf(res)
-	return fmt.Sprintf("Data has been successfully updated."), http.StatusOK
-}
-
-// PostData performs the standard post mechanism
-// TO DO : documentaion will be updated soon
-func PostData(fn func(*sql.Tx, map[string]string) *sql.Row, w http.ResponseWriter, r *http.Request) (interface{}, int) {
-	Printf("[DATABASE_HELPER] started inserting data ...")
-	defer Printf("[DATABASE_HELPER] finished inserting data ...")
-
-	tx, err := BeginTransaction()
-
-	if err != nil {
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	var values map[string]string
-	values = make(map[string]string)
-
-	params := mux.Vars(r)
-
-	values = PopulateSingleValueQueries(r, values)
-	values = PopulateParams(params, values)
-
-	var ID int
-
-	err = fn(tx, values).Scan(&ID)
-
-	if err != nil {
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	err = tx.Commit()
-
-	if err != nil {
-		tx.Rollback()
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	return fmt.Sprintf("Data has been successfully created, id=%v", ID), http.StatusOK
-}
-
-// GetData performs the standard get mechanism
-// TO DO : documentaion will be updated soon
-func GetData(fn func(map[string]string) (*sql.Rows, error), w http.ResponseWriter, r *http.Request) (interface{}, int) {
-	Printf("[DATABASE_HELPER] started getting data ...")
-	defer Printf("[DATABASE_HELPER] finished getting data ...")
-
-	var conditions map[string]string
-	conditions = make(map[string]string)
-
-	params := mux.Vars(r)
-
-	conditions = PopulateSingleValueQueries(r, conditions)
-	conditions = PopulateParams(params, conditions)
-
-	rows, err := fn(conditions)
-
-	if err != nil {
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	data, err := Scan(rows)
-
-	if err != nil {
-		return err.Error(), http.StatusInternalServerError
-	}
-
-	if data != nil {
-		return data, http.StatusOK
-	}
-
-	return data, http.StatusNotFound
 }
--- http.go
+++ http.go
@@ -5,28 +5,39 @@
 package crema
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 // PopulateSingleValueQueries populates query strings from the incoming requests
 // TO DO: documentation will be updated soon
-func PopulateSingleValueQueries(r *http.Request, conditions map[string]string) map[string]string {
+func PopulateSingleValueQueries(r *http.Request, conditions map[string]string) {
 	r.ParseForm()
 	request := r.Form
 
 	for key, val := range request {
 		conditions[key] = val[0]
 	}
-
-	return conditions
 }
 
 // PopulateParams populates the URI segment params from the incoming requests
 // TO DO: documentation will be updated soon
-func PopulateParams(params map[string]string, conditions map[string]string) map[string]string {
+func PopulateParams(params map[string]string, conditions map[string]string) {
 	for key, val := range params {
 		conditions[key] = val
 	}
+}
 
-	return conditions
+// PopulateRequestBody populates the body from the incoming requests
+// TO DO: documentation will be updated soon
+func PopulateRequestBody(r *http.Request, conditions map[string]string) {
+	var rBody map[string]interface{}
+
+	err := json.NewDecoder(r.Body).Decode(&rBody)
+
+	HandleError(err)
+
+	for key, val := range interfaceToMapStringString(rBody) {
+		conditions[key] = val
+	}
 }
