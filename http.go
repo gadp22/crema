867b4a1560306a04d2170f539c344345cb289f67
apriagung <ardhipriagung@universitaspertamina.ac.id>
2019-05-17 10:31:14 +0700

    [BUGFIX] Error requesting without raw body when using generic handler enable raw body

--- http.go
+++ http.go
@@ -5,28 +5,43 @@
 package crema
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
 // PopulateSingleValueQueries populates query strings from the incoming requests
 // TO DO: documentation will be updated soon
-func PopulateSingleValueQueries(r *http.Request, conditions map[string]string) map[string]string {
+func PopulateSingleValueQueries(r *http.Request, conditions map[string]string) {
 	r.ParseForm()
 	request := r.Form
 
 	for key, val := range request {
 		conditions[key] = val[0]
 	}
-
-	return conditions
 }
 
 // PopulateParams populates the URI segment params from the incoming requests
 // TO DO: documentation will be updated soon
-func PopulateParams(params map[string]string, conditions map[string]string) map[string]string {
+func PopulateParams(params map[string]string, conditions map[string]string) {
 	for key, val := range params {
 		conditions[key] = val
 	}
+}
 
-	return conditions
+// PopulateRequestBody populates the body from the incoming requests
+// TO DO: documentation will be updated soon
+func PopulateRequestBody(r *http.Request, conditions map[string]string) {
+	body, err := ioutil.ReadAll(r.Body)
+	if len(body) > 0 {
+		var test interface{}
+		err = json.Unmarshal(body, &test)
+
+		HandleError(err)
+
+		for key, val := range interfaceToMapStringString(test.(map[string]interface{})) {
+			conditions[key] = val
+		}
+	}
+
 }
